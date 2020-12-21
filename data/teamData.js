629e2bc58898336b3ac92866241ffa4f55faad34
Chau Vu <chv004@ucsd.edu>
2020-12-21 14:37:39 -0800

    Separated data and script

---
+++ data/teamData.js
@@ -0,0 +1,104 @@
+const teamJsonStr =`[
+    {
+        "name": "Chau Vu",
+        "photo": "./media/chau.jpg",
+        "2t1l": "I've never broken a bone in my life 🦴, I have a black belt in Taekwondo 🥋, I played chess competitively and went to nationals ♟",
+        "tech": "Full stack development",
+        "website": "http://cqvu.github.io/",
+        "github": "https://github.com/cqvu",
+        "linkedin": "https://www.linkedin.com/in/cqvu/",
+        "twitter": "https://twitter.com/cqvu_",
+        "award": "Highest of Fives"
+    },
+    {
+        "name": "Saurabh Kumar Suryan",
+        "photo": "./media/saurabh.jpg",
+        "2t1l": "🐤 The design lead of this website is very lazy 😴, belongs to Percy Jackson fandom 🔱, owns a golden retriever 🐶",
+        "tech": "Python, Flask, Docker, JavaScript, React, Node, CSS, Adobe XD",
+        "website": "https://sksuryan.me/",
+        "github": "https://github.com/sksuryan",
+        "linkedin": "https://www.linkedin.com/in/saurabh-kumar-suryan-01684b194/",
+        "twitter": "https://twitter.com/_sksuryan",
+        "award": "Meme Lord"
+    },
+    {
+        "name": "Ahad Zai",
+        "photo": "./media/ahad.jpg",
+        "2t1l": "Won 3 gold medals in boxing 🥊, Won 3 gold medals in soccer ⚽, Addicted to Dota 2 🎮",
+        "tech": "Python (Flask) and JavaScript (React)",
+        "website": "http://ahadzai.com",
+        "github": "https://github.com/ahadkhan98",
+        "linkedin": "https://www.linkedin.com/in/ahadzai/",
+        "twitter": "",
+        "award": "Comfy Standups"
+    },
+
+    {
+        "name": "Kunal Kushwaha",
+        "photo": "./media/kunal.jpg",
+        "2t1l": "I have never seen Star Wars, I have won International Art Contests, I don't like Ice Cream",
+        "tech": "All things Cloud Native",
+        "website": "http://kunal-kushwaha.github.io",
+        "github": "https://github.com/kunal-kushwaha",
+        "linkedin": "https://www.linkedin.com/in/kunal-kushwaha/",
+        "twitter": "https://twitter.com/kush_kunal",
+        "award": "I was on Mute"
+    },
+    {
+        "name": "Emily Amspoker",
+        "photo": "./media/emily.png",
+        "2t1l": "I have a one-eyed dog, I do robotics at my school, I used to play the classical guitar",
+        "tech": "Python, Java, JavaScript, HTML/CSS",
+        "website": "http://emilyamspoker.com",
+        "github": "https://github.com/eamspoker",
+        "linkedin": "https://www.linkedin.com/in/emily-amspoker-52944b18a/",
+        "twitter": "https://twitter.com/AmspokerEmily",
+        "award": "Team Player"
+    },
+    {
+        "name": "Vividha",
+        "photo": "./media/vividha.jpg",
+        "2t1l": "I love to travel & I love dogs and cats!",
+        "tech": "Python, Javascript for backend and Bootstrap for frontend",
+        "website": "https://v2dha.github.io/whyiamawesome/",
+        "github": "https://github.com/V2dha",
+        "linkedin": "https://www.linkedin.com/in/vividha-rawat-761905143/",
+        "twitter": "https://twitter.com/vvdha",
+        "award": "Can you hear me?"
+    },
+    {
+        "name": "Shilpita Biswas",
+        "photo": "./media/shilpita.jpg",
+        "2t1l": "I was born in the US, I used to be a dancer, My dad met Maroon 5",
+        "tech": "Python",
+        "website": "",
+        "github": "https://github.com/sh-biswas",
+        "linkedin": "https://www.linkedin.com/in/shilpita-biswas/",
+        "twitter": "https://twitter.com/shilpita_biswas",
+        "award": "Best Zoom Background"
+    },
+    {
+        "name": "Ajwad Shaikh",
+        "photo": "./media/ajwad.jpg",
+        "2t1l": "I can read more than 5 languages, I write poetry, I have had a pet",
+        "tech": "Just about anything that makes sense",
+        "website": "https://ajwad-shaikh.github.io",
+        "github": "https://github.com/ajwad-shaikh",
+        "linkedin": "https://www.linkedin.com/in/ajwad-shaikh/",
+        "twitter": "https://twitter.com/AjwadShaikh3",
+        "award": "The Pair Programmer"
+    },
+    {
+        "name": "Mondale Felix",
+        "photo": "./media/mondale.png",
+        "2t1l": "I can play the guitar, my favorite fruit is a mango, I am a nerd",
+        "tech": "Swift, Python",
+        "website": "",
+        "github": "https://github.com/MondaleFelix",
+        "linkedin": "https://www.linkedin.com/in/mondalefelix/",
+        "twitter": "https://twitter.com/Mondeezy",
+        "award": "Skribbl King"
+    }
+]`
+
+export { teamJsonStr };
